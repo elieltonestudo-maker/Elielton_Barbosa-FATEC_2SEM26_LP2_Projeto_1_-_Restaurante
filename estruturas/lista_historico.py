bfd8208338e3a6c053bdf1eb0a2e26b637e7301c
elielton <elielton.estudo@gmail.com>
2026-09-13 00:39:17 -0300

    Update lista_historico.py

--- estruturas/lista_historico.py
+++ estruturas/lista_historico.py
@@ -4,16 +4,17 @@ from estruturas.no import No
 
 class ListaHistorico:
     def __init__(self):
-        self.cabeca = None
+        self.cabeca = None  # Início do histórico de pagamentos
 
     def adicionar_registro(self, registro_pagamento):
+        """Insere um novo registro de pagamento no início da lista de histórico."""
         novo_no = No(registro_pagamento)
         novo_no.proximo = self.cabeca
-        self.cabeca = novo_no
+        self.cabeca = novo_no # Vai para a cabeça.
 
     def obter_todos(self):
-        """Retorna os registros sequencialmente (CUIDADO: bug de laco infinito ativo)."""
+        """Retorna um gerador para percorrer os registros sequencialmente na hora do relatório."""
         atual = self.cabeca
         while atual is not None:
             yield atual.conteudo
-           
+            atual = atual.proximo # Avança para o próximo nó.
